e97f32c3b26ca95688e950aa83cd756bad09ac8e
vinx <kevinmocorro@gmail.com>
2017-08-17 15:17:42 +0800

    New script for All outs

--- script/SQL_inserter_run.sh
+++ script/SQL_inserter_run.sh
@@ -3,6 +3,9 @@
 # KEVIN B MOCORRO
 # Version 1.0
 
+cd /c/sandbox/nodejs-web-app/script
+PATH=$PATH:/c/xampp/mysql/bin
+
  # Will insert target every morning
  mysql -h localhost -u root -p2qhls34r dbtarget < inserter.sql
 
