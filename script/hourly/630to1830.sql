d46922404e113e1e7f9fd8eebbb459e2242adaed
vinx <kevinmocorro@gmail.com>
2017-09-06 15:04:52 +0800

    Optimized App

---
+++ script/hourly/630to1830.sql
@@ -0,0 +1,128 @@
+SET time_zone ='+08:00';
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'DAMAGE' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'POLY' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+  
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'BSGDEP' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'NTM' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'NOXE' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'NOXE' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'NDEP' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'PTM' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'TOXE' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+  
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'CLEANTEX' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'PDRIVE' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'ARC_BARC' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'PBA' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'LCM' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+  
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'SEED' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'FGA' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+
+
+ 
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'PLM' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+  
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'EDG_CTR' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+  
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'PLATING' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+  
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'ETCHBK' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+  
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'HST' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
+ 
+  
+UNION ALL 
+
+SELECT process_id, SUM(out_qty) AS totalOuts FROM MES_OUT_DETAILS WHERE process_id = 'TEST' 
+ AND date_time >= CONCAT(CURDATE()," 06:30:00") AND date_time <= CONCAT(CURDATE()," 18:29:59")
